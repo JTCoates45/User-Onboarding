ad4d57298d0c608bfff2ff2383a8600991a361df
JT Coates <wyosred@yahoo.com>
2022-04-27 23:00:23 -0600

    finished app.js

---
+++ user-onboarding/src/Component/Form.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Form = (props) => {
+    const {change, submit} = props;
+    const {name, email, password, ToS, checked} = props.values;
+
+    const onChange = (evt) => {
+        const {name,value,checked,type} = evt.target
+        const newValue = type === 'checkbox' ? checked : value;
+        change (name, newValue);
+    }
+
+    const onSubmit = (evt) => {
+        evt.preventDefualt();
+        submit();
+    }
+
+    return (
+        <div>
+        <h1>Form</h1>
+        <form onSubmit={onSubmit}>
+            <label>Name:
+                <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={onChange}
+                />
+            </label>
+            <label>Email:
+            <input
+                type="text"
+                name="email"
+                value={email}
+                onChange={onChange}
+                />
+            </label>
+            <label>Password
+            <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={onChange}
+                />
+            </label>
+            <label>Terms of Service
+            <input
+                type="checkbox"
+                name="ToS"
+                checked={checked}
+                onChange={onChange}
+                />
+            </label>
+            <input type="submit" value="Create Form" />
+        </form>
+        </div>
+    )
+}
+
+
+export default Form;
